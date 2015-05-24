ad36bae1a97e105b480161635f6534528f88efb0
Elliot Grabish <egrabish@gmail.com>
2015-05-24 15:06:39 -0400

    first commit

---
+++ header.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+	<meta name="viewport" content="width=device-width" />
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link href='http://fonts.googleapis.com/css?family=Lobster' rel='stylesheet' type='text/css'>
+	<link href='http://fonts.googleapis.com/css?family=Righteous' rel='stylesheet' type='text/css'>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+	<title>Elliot Grabish Online</title>
+</head>
+
+<body>
+
+	<!-- HEADER -->
+
+	<!--Mobile Menu -->
+
+	<div id="menuOverlay">
+		<div class="burger" style="margin-top: 20px">
+		</div>
+	</div>
+
+	<header>
+
+		<div class="backTitle">
+			<h1> My Work </h1>
+		</div>
+
+		<!-- NAVIGATION -->
+
+		<!-- Hamburger Icon -->
+
+		<div class="burger">
+		</div>
+
+		<nav>
+
+			<ul class="ulLeft">
+				<li id="mywork"><a href="#">My Work</a></li>
+				<li id="aboutme"><a href="#">About Me</a></li>
+			</ul>
+			<ul class="ulRight">
+				<li id="blog"><a href="#">Blog</a></li>
+				<li id="contact"><a href="#">Contact</a></li>
+			</ul>
+
+		</nav>
+
+		<!-- LOGO -->
+
+		<div id="logo">
+
+			<img src="images/newlogo2none.png" title="EG Logo" alt="EG Logo" class="top_image"/>
+			
+			<!-- Fade below image in on load -->
+			<img src="images/newlogo2.png" title="EG Logo" alt="EG Logo" class="bottom_image" />
+
+		</div>
+
+	</header>
+
+<script type="text/javascript">
+
+	// JS for active link
+
+	$(document).ready(function () {
+    	if(window.location.href.indexOf("index") > -1) {
+       		var myWork = document.getElementById('mywork');
+       		myWork.style.AnimationName = "gif";
+       		myWork.style.webkitAnimationName = "gif";
+
+       		myWork.style.AnimationDuration = '1s';
+       		myWork.style.webkitAnimationDuration = '1s';
+
+       		myWork.style.animationIterationCount = 'infinite';
+  			myWork.style.webkitAnimationIterationCount = 'infinite';
+    	}
+
+    	if(window.location.href.indexOf("aboutme") > -1) {
+       		var myWork = document.getElementById('mywork');
+       		myWork.style.AnimationName = "giffff";
+       		myWork.style.webkitAnimationName = "giffff";
+
+       		myWork.style.AnimationDuration = '1s';
+       		myWork.style.webkitAnimationDuration = '1s';
+
+       		myWork.style.animationIterationCount = 'infinite';
+  			myWork.style.webkitAnimationIterationCount = 'infinite';
+    	}
+
+    	if(window.location.href.indexOf("blog") > -1) {
+       		var myWork = document.getElementById('mywork');
+       		myWork.style.AnimationName = "gifff";
+       		myWork.style.webkitAnimationName = "gifff";
+
+       		myWork.style.AnimationDuration = '1s';
+       		myWork.style.webkitAnimationDuration = '1s';
+
+       		myWork.style.animationIterationCount = 'infinite';
+  			myWork.style.webkitAnimationIterationCount = 'infinite';
+    	}
+
+    	if(window.location.href.indexOf("contact") > -1) {
+       		var myWork = document.getElementById('mywork');
+       		myWork.style.AnimationName = "giff";
+       		myWork.style.webkitAnimationName = "giff";
+
+       		myWork.style.AnimationDuration = '1s';
+       		myWork.style.webkitAnimationDuration = '1s';
+
+       		myWork.style.animationIterationCount = 'infinite';
+  			myWork.style.webkitAnimationIterationCount = 'infinite';
+    	}
+
+    	//MOBILE MENU
+
+    	$('#menuOverlay').hide()
+	
+		$('.burger').click(function(){
+
+			//Jquery incase Vh does not work for menu div
+			var clientHeight = $( window ).height();
+  			$('#menuOverlay').css('height', clientHeight);
+
+			$('#menuOverlay').fadeToggle(1000);
+		});
+
+  //   	function AnimateRotate(d){
+  //   		$({deg: 0}).animate({deg: d}, {
+  //       		step: function(now, fx){
+  //           		$("#logo").css({
+  //                		transform: "rotate(" + now + "deg)";
+  //                		width: 250px;
+  //                		height: 100px;
+  //           		});
+  //       		}
+  //   		});
+		// }
+
+		// AnimateRotate(90);
+	});
+
+</script>
---
+++ index.php
@@ -0,0 +1,101 @@
+<?php include('header.php'); ?>
+
+	<div class="clear"></div>
+
+	<!-- MAIN CONTENT -->
+
+	<main role="main">
+
+		<!-- <div class="innerTitle">
+			<h1> My Work </h1>
+		</div> -->
+
+		<div class="wholeProject">
+
+			<div class="leftCol">
+				<div class="proj">
+					<div class="projLogo">
+						<img src="images/altanlogo.png" title="Altan Logo" alt="Altan Logo" />
+					</div>
+				</div>
+			</div>
+
+			<div class="rightCol">
+				<div class="projRight">
+					<div class="projScreen">
+						<img src="images/altanpage.jpg" title="Altan Page" alt="Altan Page" />
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+		<div class="wholeProject">
+
+			<div class="leftCol">
+				<div class="proj">
+					<div class="projLogo">
+						<img src="images/torontofacillogo.png" title="Toronto Facilities Logo" alt="Toronto Facilities Logo" />
+					</div>
+				</div>
+			</div>
+
+			<div class="rightCol">
+				<div class="projRight">
+					<div class="projScreen">
+						<img src="images/torontoparkpage.jpg" title="Toronto Facilities Page" alt="Toronto Facilities Page" />
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+		<div class="wholeProject">
+
+			<div class="leftCol">
+				<div class="proj">
+					<div class="projLogo">
+						<img src="images/notredamelogo.png" title="Notre Dame Logo" alt="Notre Dame Logo" />
+					</div>
+				</div>
+			</div>
+
+			<div class="rightCol">
+				<div class="projRight">
+					<div class="projScreen">
+						<img src="images/notredamepage.jpg" title="Notre Dame Page" alt="Notre Dame Page" />
+					</div>
+				</div>
+			</div>
+
+		</div>
+
+	</main>
+
+	<!-- FOOTER -->
+
+	<!-- <footer>
+
+		<div id="page_info">
+
+			<p>&copy; Elliot Grabish</p>
+			<p>Junior Interactive Developer Exercise - Option 2</p>
+
+		</div>
+
+		<div id="footer_links">
+
+			<ul>
+				<li><a href="#">Home</a></li>
+				<li><a href="#">About</a></li>
+				<li><a href="#">Services</a></li>
+				<li><a href="#">Contact</a></li>
+			</ul>
+
+		</div>
+
+	</footer> -->
+
+</body>
+
+</html>
