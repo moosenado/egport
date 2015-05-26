38b9778264157c38ad13943fb86d8dd783a65fdf
Elliot Grabish <egrabish@gmail.com>
2015-05-26 18:31:32 -0400

    altan clean

--- altan.php
+++ altan.php
@@ -3,8 +3,9 @@
 	<main role="main">
 
 		<div id="portMain">
+
 			<div class="portImage">
-			<img src="images/altanlogo.png" title="Altan Logo" alt="Altan Logo" />
+				<img src="images/altanlogo.png" title="Altan Logo" alt="Altan Logo" />
 			</div>
 
 			<div class="innerTitle">
@@ -13,26 +14,26 @@
 
 			<div class="portInfo">
 
-			<h2 class="titlePadding">Goal</h2>
+				<h2 class="titlePadding">Goal</h2>
 
-			<p class="justify">
-				Rebrand Altan Safe Outdoors' online presence through a completely redesigned website including an online order quote application. Website must be cross device and browser compatible (IE6+).
-			</p>
+				<p class="justify">
+					Rebrand Altan Safe Outdoors' online presence through a completely redesigned website including an online order quote application. Website must be cross device and browser compatible (IE6+).
+				</p>
 
-			<h2 class="titlePadding">Technologies</h2>
+				<h2 class="titlePadding">Technologies</h2>
 
-			<div id="techList">
+				<div id="techList">
 
-				<ul>
-					<li>HTML</li>
-					<li>CSS</li>
-					<li>JavaScript</li>
-					<li>JQuery</li>
-					<li>ASP.NET</li>
-					<li>C#</li>
-				</ul>
+					<ul>
+						<li>HTML</li>
+						<li>CSS</li>
+						<li>JavaScript</li>
+						<li>JQuery</li>
+						<li>ASP.NET</li>
+						<li>C#</li>
+					</ul>
 
-			</div>
+				</div>
 
 			</div>
 
@@ -41,21 +42,25 @@
 			</div>
 
 			<h2 class="titlePadding">Desktop Design</h2>
+
 			<div class="portImage">
 				<img src="images/altanpage.jpg" title="Altan Page" alt="Altan Page" />
 			</div>
 
 			<h2 class="titlePadding">Mobile Design</h2>
+
 			<div class="portImageMobile">
 				<img src="images/altanpagemobile.jpg" title="Altan Page Mobile" alt="Altan Page Mobile" />
 			</div>
 
 			<h2 class="titlePadding">Graphic Design</h2>
+
 			<div class="portImageMobile">
 				<img src="images/altanad.jpg" title="Altan Ad" alt="Altan Ad" />
 			</div>
 
 			<h2 class="titlePadding">GIF Animation</h2>
+			
 			<div class="portImage">
 				<img src="images/altangif.gif" title="Altan Animation" alt="Altan Animation" />
 			</div>
